a0e660b0bbb9caf4c6c9583344def28aa8066699
studiobuttermedia <bebosua05@yahoo.com.vn>
2023-08-14 18:51:34 +0700

    Added a Test Slash command. And some notes.

--- Status.md
+++ Status.md
@@ -1,5 +1,7 @@
 # Project Status
 
+When setting the bot, must use `bot` and `application_commands` scopes before passing the bot to a server.
+
 Honkai Impact 3rd doesn't have update Packages and No Language Pack. Users will only need to choose the region for the game
 
 Genshin Impact has Update Pack and Game Language Pack which both can be update. User will need to choose their region version, type of Installation, their language pack of choice and what version they updating from if they choose Update Pack instead of Full Install Pack, with either 3.7.0 to 4.0.0 or 3.8.0 to 4.0.0.
--- bot.py
+++ bot.py
@@ -1,21 +1,33 @@
 import discord
 from dotenv import load_dotenv
 from discord import app_commands
-from discord.ext import commands
+# from discord.ext import commands
 import os
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild = discord.Object(id = 871644064385216613))
+            self.synced = True
+        await self.change_presence(status=discord.Status.dnd, activity=discord.Game('Star Wrench'))
+        print(f'Star Wrench Bot is Online!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+# This area is where the bot runs
+client = MyClient()
+tree = app_commands.CommandTree(client)
 client.run(TOKEN) # type: ignore
+
+@tree.command(name = "test", description = "Test command", guild = discord.Object(id = 871644064385216613))
+async def self(interaction: discord.Interaction): # type: ignore
+    await interaction.response.send_message("Test command works!") # , ephemeral = True)
