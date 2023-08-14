c63ed40241183205401d5ef911d4cf35a67c9532
studiobuttermedia <bebosua05@yahoo.com.vn>
2023-08-14 17:24:24 +0700

    Change so Enviroment Variables starts first

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
+from dotenv import load_dotenv
 from discord import app_commands
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
