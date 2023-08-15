6c5726e84d25ffe436d604c1c777c82fa1e474d7
studiobuttermedia <bebosua05@yahoo.com.vn>
2023-08-15 13:34:02 +0700

    Adding a call for Pre_Install API

---
+++ api_call_pre.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import os
+
+if os.path.isfile("pre_data.txt") == True:
+    file = open("pre_data.txt", "r")
+    content = file.read()
+    file.close()
+    if content:
+        print("Cleaning existing data")
+        file = open("pre_data.txt", "w")
+        file.write("")
+        file.close()
+    else:
+        print("No data found in Text file")
+else:
+    print("Creating file")
+    file = open("pre_data.txt", "a")
+    file.close()
+
+
+print("Getting Requested content")
+
+url = 'https://sdk-os-static.mihoyo.com/hk4e_global/mdk/launcher/api/resource'
+headers = {'Accept': 'application/json'}
+params = {
+    "channel_id": "1",
+    "key": "gcStgarh",
+    "launcher_id": "10",
+    "sub_channel_id": "0"
+}
+
+r = requests.get(url, params, headers=headers)
+
+# Add a check for status code
+
+if r.status_code == 200:
+    content = json.loads(r.text)
+    for sys1 in content['data']['pre_download_game']['latest']['segments']:
+        # Get URL Links from JSON dict ['data']['game']['latest']['segments']['path'] and write to text file with numbers on each line
+        file = open("pre_data.txt", "a")
+        file.write(sys1['path'] + "\n")
+        file.close()
+else:
+    print(f"Error: {r.status_code}")
